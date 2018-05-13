a00b28a0d30b565aede1012844b918a8e64bfb43
keys961 <keys961@hotmail.com>
2018-05-13 21:00:47 +0800

    Update first comment processor

--- comment.cpp
+++ comment.cpp
@@ -4,17 +4,120 @@
 
 #include "comment.h"
 
-std::string CommemtProcessor::processLine(std::string in)
-{
-    unsigned int index = in.find("//");
-    return in.substr(0, index);
-}
-
 void CommemtProcessor::processComment(std::istream &in, std::ostream &out)
 {
+    writeContent(out, deleteComment(readContent(in)));
+}
+
+std::string CommemtProcessor::deleteComment(const std::string& in)
+{
+    std::string result;
+    State state = CODE;
+
+    for (char c : in) {
+        switch (state)
+        {
+            case CODE:
+                if(c == '/')
+                    state = SLASH;
+                else
+                {
+                    result += c;
+                    if(c == '\'')
+                        state = CODE_CHAR;
+                    else if(c == '\"')
+                        state = CODE_STRING;
+                }
+                break;
+            case SLASH:
+                if(c == '*')
+                    state = COMMENT_MULTILINE;
+                else if (c == '/')
+                    state = COMMENT_SINGLE_LINE;
+                else
+                {
+                    result += '/';
+                    result += c;
+                    state = CODE;
+                }
+                break;
+            case COMMENT_MULTILINE:
+                if(c == '*')
+                    state = COMMENT_MULTILINE_STAR;
+                else
+                {
+                    if(c == '\n')
+                        result += "\r\n";
+                }
+                break;
+            case COMMENT_MULTILINE_STAR:
+                if(c == '/')
+                    state = CODE;
+                else if(c == '*')
+                    state = COMMENT_MULTILINE_STAR;
+                else
+                    state = COMMENT_MULTILINE;
+                break;
+            case COMMENT_SINGLE_LINE:
+                if (c == '\\')
+                    state = BACKSLASH;
+                else if (c == '\n')
+                {
+                    result += "\r\n";
+                    state = CODE;
+                }
+                break;
+            case BACKSLASH:
+                if(c == '\\' || c == '\r' || c == '\n')
+                {
+                    if(c == '\n')
+                        result += "\r\n";
+                }
+                else
+                    state = COMMENT_SINGLE_LINE;
+                break;
+            case CODE_CHAR:
+                result += c;
+                if(c == '\\')
+                    state = CHAR_ESCAPE_SEQUENCE;
+                else if (c == '\'')
+                    state = CODE;
+                break;
+            case CHAR_ESCAPE_SEQUENCE:
+                result += c;
+                state = CODE_CHAR;
+                break;
+            case CODE_STRING:
+                result += c;
+                if(c == '\\')
+                    state = STRING_ESCAPE_SEQUENCE;
+                else if(c == '\"')
+                    state = CODE;
+                break;
+            case STRING_ESCAPE_SEQUENCE:
+                result += c;
+                state = CODE_STRING;
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    return result;
+}
+
+std::string CommemtProcessor::readContent(std::istream &in)
+{
+    std::string content;
     std::string line;
     while(std::getline(in, line))
-        out << processLine(line) << std::endl;
-    out.flush();
+        content += line + "\n";
+    return content;
+}
+
+void CommemtProcessor::writeContent(std::ostream &out, const std::string &content)
+{
+    out << content << std::endl;
 }
 
--- comment.h
+++ comment.h
@@ -8,10 +8,30 @@
 #include <iostream>
 #include <string>
 
+/**
+ * Referenced from https://www.cnblogs.com/mq0036/p/7942878.html
+ */
+
+enum State
+{
+    CODE,
+    SLASH,
+    COMMENT_MULTILINE,
+    COMMENT_MULTILINE_STAR,
+    COMMENT_SINGLE_LINE,
+    BACKSLASH,
+    CODE_CHAR,
+    CHAR_ESCAPE_SEQUENCE,
+    CODE_STRING,
+    STRING_ESCAPE_SEQUENCE
+};
+
 class CommemtProcessor
 {
 private:
-    std::string processLine(std::string in);
+    std::string deleteComment(const std::string& in);
+    std::string readContent(std::istream& in);
+    void writeContent(std::ostream& out, const std::string& content);
 public:
     void processComment(std::istream& in, std::ostream& out);
 };
